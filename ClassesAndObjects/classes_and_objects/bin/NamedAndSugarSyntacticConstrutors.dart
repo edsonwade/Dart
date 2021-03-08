118fde30c3a11b25a51fec952279389086801cb4
vanil <vanil@DESKTOP-EPO41LJ>
2021-03-08 01:14:57 +0000

    intensselij

--- ClassesAndObjects/classes_and_objects/bin/NamedAndSugarSyntacticConstrutors.dart
+++ ClassesAndObjects/classes_and_objects/bin/NamedAndSugarSyntacticConstrutors.dart
@@ -20,13 +20,21 @@ class Microphone {
   }
   // this = this objec /class
 
+  String getName(name) => name;
+
+  void setName(String name) => this.name = name;
+
   sum(num1, num2) => num1 + num2;
 }
 
 void main(List<String> arguments) {
   var mic = Microphone('Vanilson', 'blue silver', 1234);
-   
-   var micSecond = Microphone.initialize();
+// used the initialize named construtor
+  var micSecond = Microphone.initialize();
+  
+
+
 
   print('the sum result is :${mic.sum(23, 4)}');
+  print('the sum result is :${micSecond.name}');
 }
